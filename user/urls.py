736767d086e0ff326e46666ea24297b916f095ba
BizzareNut <mqq170066@outlook.com>
2023-03-10 21:44:55 +0000

    Feat: Update the user's information by some conditions. Feat: Delete the user by some conditions.

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
 
-from user.views import add_user, login_user
-
+from user.views import add_user, login_user, update_user, delete_user
 urlpatterns = [
     path('signup/', add_user, name="signup"),
     path('login/', login_user, name="login"),
+    path('UpdateUserInfo/', update_user, name="UpdateUserInfo"),
+    path('delete/', delete_user, name="delete_account"),
 ]
--- user/views.py
+++ user/views.py
@@ -18,6 +18,7 @@ from user.utils import get_tokens_for_user
 def add_user(request):
     response = {}
     js = json.loads(request.body)
+    #查找
     res_email = User.objects.filter(email = js['email']).count()
     res_phone = User.objects.filter(phone = js['phone']).count()
     if res_email != 0:
@@ -62,3 +63,68 @@ def login_user(request):
 
     return JsonResponse(response)
 
+@csrf_exempt
+@require_http_methods(['PUT'])
+def update_user(request):
+    response = {}
+    js = json.loads(request.body)
+    try:
+        user = User.objects.filter(username=js['username']).get()
+
+        if 'email' in js:
+            if User.objects.filter(email=js['email']).exclude(username=user.username).exists():
+                response['msg'] = 'email already exists'
+                response['status'] = 321
+                return JsonResponse(response)
+            user.email = js['email']
+
+        if 'phone' in js:
+            if User.objects.filter(phone=js['phone']).exclude(username=user.username).exists():
+                response['msg'] = 'phone already exists'
+                response['status'] = 322
+                return JsonResponse(response)
+            user.phone = js['phone']
+
+        if 'new_username' in js:
+            if User.objects.filter(username=js['new_username']).exclude(pk=user.pk).exists():
+                response['msg'] = 'username already exists'
+                response['status'] = 323
+                return JsonResponse(response)
+            user.username = js['new_username']
+
+
+        user.save()
+
+    except User.DoesNotExist:
+        response['msg'] = 'user not found'
+        response['status'] = 31
+        return JsonResponse(response)
+
+    response['msg'] = 'success'
+    response['status'] = 0
+
+    return JsonResponse(response)
+
+
+@csrf_exempt
+@require_http_methods('DELETE')
+def delete_user(request):
+    response = {}
+    js = json.loads(request.body)
+    #查找用户
+    try:
+        # 查找用户
+        user = User.objects.filter(username=js['username']).get()
+        # user = get_object_or_404(User, username=username)
+        # 删除
+        user.delete()
+        response['msg'] = 'success'
+        response['status'] = 0
+    except User.DoesNotExist:
+        response['msg'] = 'User not found'
+        response['status'] = 31
+    except Exception as e:
+        response['msg'] = str(e)
+        response['status'] = 4
+
+    return JsonResponse(response)
