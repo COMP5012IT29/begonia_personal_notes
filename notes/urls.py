a0264be60266d8f089221ed2cd01abc91f716edf
EckCiting <eckciting@outlook.com>
2023-03-20 21:46:15 +0000

    Fix: allow user to star a note

--- notes/urls.py
+++ notes/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('editNote/',edit_note,name="update note"),
     path('showNoteList/',show_notes,name='show note list'),
     path('showHint/',view_hint,name='show note password hint'),
-    path('search/',search_note,name='search note')
+    path('search/',search_note,name='search note'),
+    path('starNote/',star_note,name="star note")
 ]
